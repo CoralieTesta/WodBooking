4bace90f9270e3fb11ba5a72937352074cce5411
CoralieTesta <coralie.testa@hotmail.com>
2023-03-21 11:18:29 +0100

    Ajout des options 'passées' et 'à venir' dans mes réservations

--- frontend/src/components/Bookings/Bookings.jsx
+++ frontend/src/components/Bookings/Bookings.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import UserContext from "../../store/user-context"
 import s from "./style.module.css"
 import { BookingItem } from "../BookingItem/BookingItem"
@@ -6,6 +6,15 @@ import { BookingItem } from "../BookingItem/BookingItem"
 export function Bookings() {
     const userCtx = useContext(UserContext)
     const bookingArray = userCtx.bookingArray
+    const [isFutureSelected, setIsFutureSelected] = useState(true)
+    const date = new Date()
+    const [month, day, year] = [
+        date.getMonth()+1,
+        date.getDate(),
+        date.getFullYear(),
+      ];
+    const todayDateInNb = day+"/"+month+"/"+year
+    console.log('today', todayDateInNb)
     console.log("array before",bookingArray)
     function getFirstPart(str) {
         return str.split('/')[0];
@@ -23,8 +32,31 @@ export function Bookings() {
         const bDate=new Date(bDateFormat)
         return aDate.getTime()-bDate.getTime()
     })
-    bookingArray.reverse()
-    console.log("array after",bookingArray)
+    console.log("booking arr",bookingArray)
+
+    const pastBookingArray = bookingArray.filter(day => {
+        const dayDateFormat =getSecondPart(day.date)+"/"+getFirstPart(day.date)+"/"+getThirdPart(day.date)
+        const dayDate=new Date(dayDateFormat)
+        console.log(day, dayDate.getTime(), date.getTime())
+        return dayDate.getTime()<date.getTime()-90000000
+    })
+    pastBookingArray.reverse()
+
+    const futureBookingArray = bookingArray.filter(day => {
+        const dayDateFormat =getSecondPart(day.date)+"/"+getFirstPart(day.date)+"/"+getThirdPart(day.date)
+        const dayDate=new Date(dayDateFormat)
+        return dayDate.getTime()>=date.getTime()-90000000
+    })
+
+    console.log("before",pastBookingArray, "fut", futureBookingArray)
+
+    function onSelectPast() {
+        setIsFutureSelected(false)
+    }
+
+    function onSelectFuture() {
+        setIsFutureSelected(true)
+    }
 
 
     if(bookingArray.length === 0) {
@@ -36,19 +68,58 @@ export function Bookings() {
     }
     return(
         <div className={s.container}>
-            <ul className={s.ul}>
-                {bookingArray.map(booking =>
-                    <li key={booking._id} className={s.li} >
-                        <div>
-                            <BookingItem 
-                            date={booking.date}
-                            startingHour={booking.startingHour}
-                            title={booking.title}
-                        />
-                        </div>
-                    </li>
-                )}
-            </ul>
+            <div className={s.buttons}>
+                <button 
+                    onClick={onSelectPast}
+                    className={s.btn}
+                    style={{
+                        borderBottom: isFutureSelected? 'none': '3px solid black',
+                        backgroundColor: isFutureSelected? 'white': '#EDEDED'
+                    }}
+                >
+                    Passées
+                </button>
+                <button 
+                    onClick={onSelectFuture}
+                    className={s.btn}
+                    style={{
+                        borderBottom: isFutureSelected? '3px solid black' : 'none',
+                        backgroundColor: isFutureSelected? '#EDEDED' : 'white'
+                    }}
+                >
+                    À venir
+                </button>
+            </div>
+            {isFutureSelected?
+                (<ul className={s.ul}>
+                    {futureBookingArray.map(booking =>
+                        <li key={booking._id} className={s.li} >
+                            <div>
+                                <BookingItem 
+                                date={booking.date}
+                                startingHour={booking.startingHour}
+                                title={booking.title}
+                            />
+                            </div>
+                        </li>
+                    )}
+                </ul>)
+                :
+                (<ul className={s.ul}>
+                    {pastBookingArray.map(booking =>
+                        <li key={booking._id} className={s.li} >
+                            <div>
+                                <BookingItem 
+                                date={booking.date}
+                                startingHour={booking.startingHour}
+                                title={booking.title}
+                            />
+                            </div>
+                        </li>
+                    )}
+                </ul>)
+
+            }
         </div>
     )
 }
